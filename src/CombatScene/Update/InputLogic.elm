cd556bdc031b64b5ae296db71527e21feb4fc557
Chris Adams <chris.adams@email.wsu.edu>
2017-01-01 20:43:17 -0800

    Fixed some formatting

--- src/CombatScene/Update/InputLogic.elm
+++ src/CombatScene/Update/InputLogic.elm
@@ -9,14 +9,24 @@ addPlayerMoveToInputFrame dir playerID inputFrame =
     case findFirst (\i -> i.playerID == playerID) inputFrame.playerInputs of
         Just playerInputFrame ->
             let
-                updatedPlayerDirX = if dir.x == 0 then playerInputFrame.moveKeys.x else dir.x
-                updatedPlayerDirY = if dir.y == 0 then playerInputFrame.moveKeys.y else dir.y
+                updatedPlayerDirX =
+                    if dir.x == 0 then
+                        playerInputFrame.moveKeys.x
+                    else
+                        dir.x
+
+                updatedPlayerDirY =
+                    if dir.y == 0 then
+                        playerInputFrame.moveKeys.y
+                    else
+                        dir.y
             in
-                { inputFrame | playerInputs = 
-                    listUpdate
-                        (\i -> i.playerID == playerID)
-                        inputFrame.playerInputs
-                        { playerInputFrame | moveKeys = {x = updatedPlayerDirX, y = updatedPlayerDirY} }
+                { inputFrame
+                    | playerInputs =
+                        listUpdate
+                            (\i -> i.playerID == playerID)
+                            inputFrame.playerInputs
+                            { playerInputFrame | moveKeys = { x = updatedPlayerDirX, y = updatedPlayerDirY } }
                 }
 
         Nothing ->
@@ -37,20 +47,23 @@ addPlayerAttackToInputFrame playerID inputFrame =
                 { inputFrame | playerInputs = updatedPlayerInputs }
 
         Nothing ->
-            inputFrame    
+            inputFrame
 
 
 updateInputState : InputFrame -> InputState -> InputState
 updateInputState inputFrame inputState =
     let
-      turnChanged = inputState.ticks >= 100
+        turnChanged =
+            inputState.ticks >= 100
     in
-        { inputState | playerInputStates =
-            if not turnChanged 
-                then List.map2 updatePlayerInputState inputFrame.playerInputs inputState.playerInputStates
-                else List.map clearPlayerInputState inputState.playerInputStates
-        , ticks = (inputState.ticks % 100) + 1
-        , turnChanged = turnChanged
+        { inputState
+            | playerInputStates =
+                if not turnChanged then
+                    List.map2 updatePlayerInputState inputFrame.playerInputs inputState.playerInputStates
+                else
+                    List.map clearPlayerInputState inputState.playerInputStates
+            , ticks = (inputState.ticks % 100) + 1
+            , turnChanged = turnChanged
         }
 
 
@@ -60,11 +73,20 @@ updateInputState inputFrame inputState =
 
 updatePlayerInputState : PlayerInputFrame -> PlayerInputState -> PlayerInputState
 updatePlayerInputState playerInputFrame oldState =
-    { oldState | moveDirection = 
-        { x = if playerInputFrame.moveKeys.x == 0 then oldState.moveDirection.x else playerInputFrame.moveKeys.x
-        , y = if playerInputFrame.moveKeys.y == 0 then oldState.moveDirection.y else playerInputFrame.moveKeys.y
-        }
-    , attack = oldState.attack || playerInputFrame.attackBtn
+    { oldState
+        | moveDirection =
+            { x =
+                if playerInputFrame.moveKeys.x == 0 then
+                    oldState.moveDirection.x
+                else
+                    playerInputFrame.moveKeys.x
+            , y =
+                if playerInputFrame.moveKeys.y == 0 then
+                    oldState.moveDirection.y
+                else
+                    playerInputFrame.moveKeys.y
+            }
+        , attack = oldState.attack || playerInputFrame.attackBtn
     }
 
 
@@ -78,4 +100,4 @@ clearPlayerInputState inputState =
 
 clearInputFrame : InputFrame -> InputFrame
 clearInputFrame inputFrame =
-   { inputFrame | playerInputs = List.map (\playerInputFrame -> {playerInputFrame | moveKeys = zeroVector, attackBtn = False }) inputFrame.playerInputs }
+    { inputFrame | playerInputs = List.map (\playerInputFrame -> { playerInputFrame | moveKeys = zeroVector, attackBtn = False }) inputFrame.playerInputs }
